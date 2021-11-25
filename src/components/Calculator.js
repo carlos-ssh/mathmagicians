eb28b56d292bfa3d812f5ccb27ba7350941ac353
Carlos <carlos.anezeh@gmail.com>
2021-11-24 19:09:33 -0600

    Add style to buttons.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -12,41 +12,117 @@ class Calculator extends Component {
       <>
         <Row className="row d-flex justify-content-center">
           <Col xs={12} className="w-100">
-            <Button variant="btn btn-primary">AC</Button>
-            <Button>+/-</Button>
-            <Button>%</Button>
-            <Button>÷</Button>
+            <Button
+              variant="btn btn-secondary col-2 border-dark"
+            >
+              AC
+            </Button>
+            <Button
+              variant="btn btn-secondary col-2 border-dark"
+            >
+              +/-
+            </Button>
+            <Button
+              variant="btn btn-secondary col-2 border-dark"
+            >
+              %
+            </Button>
+            <Button
+              variant="btn btn-secondary col-2 border-dark"
+            >
+              ÷
+            </Button>
           </Col>
         </Row>
-        <Row>
+        <Row className="row d-flex justify-content-center">
           <Col xs={12} className="w-100">
-            <Button>7</Button>
-            <Button>8</Button>
-            <Button>9</Button>
-            <Button>x</Button>
+            <Button
+              variant="btn btn-success col-2 border-dark"
+            >
+              7
+            </Button>
+            <Button
+              variant="btn btn-success col-2 border-dark"
+            >
+              8
+            </Button>
+            <Button
+              variant="btn btn-success col-2 border-dark"
+            >
+              9
+            </Button>
+            <Button
+              variant="btn btn-secondary col-2 border-dark"
+            >
+              x
+            </Button>
           </Col>
         </Row>
-        <Row>
+        <Row className="row d-flex justify-content-center">
           <Col xs={12} className="w-100">
-            <Button>4</Button>
-            <Button>5</Button>
-            <Button>6</Button>
-            <Button>-</Button>
+            <Button
+              variant="btn btn-success col-2 border-dark"
+            >
+              4
+            </Button>
+            <Button
+              variant="btn btn-success col-2 border-dark"
+            >
+              5
+            </Button>
+            <Button
+              variant="btn btn-success col-2 border-dark"
+            >
+              6
+            </Button>
+            <Button
+              variant="btn btn-secondary col-2 border-dark"
+            >
+              -
+            </Button>
           </Col>
         </Row>
-        <Row>
+        <Row className="row d-flex justify-content-center">
           <Col xs={12} className="w-100">
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
-            <Button>+</Button>
+            <Button
+              variant="btn btn-success col-2 border-dark"
+            >
+              1
+            </Button>
+            <Button
+              variant="btn btn-success col-2 border-dark"
+            >
+              2
+            </Button>
+            <Button
+              variant="btn btn-success col-2 border-dark"
+            >
+              3
+            </Button>
+            <Button
+              variant="btn btn-secondary col-2 border-dark"
+            >
+              +
+            </Button>
           </Col>
         </Row>
-        <Row>
+        <Row className="row d-flex justify-content-center">
           <Col xs={12} className="w-100">
-            <Button> 0 </Button>
-            <Button>.</Button>
-            <Button>=</Button>
+            <Button
+              variant="btn btn-success col-4 border-dark"
+            >
+              0
+            </Button>
+            <Button
+              variant="btn btn-success col-2 border-dark"
+            >
+              .
+            </Button>
+            <Button
+              variant="btn btn-secondary col-2 border-dark"
+            >
+              =
+            </Button>
           </Col>
         </Row>
       </>
